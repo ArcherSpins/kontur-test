c84de25afbd47a6b1358e677a5630725a2d26853
ArcherSpins <kambaevahmed@list.ru>
2020-01-10 10:43:27 +0300

    init

--- client.js
+++ client.js
@@ -1,7 +1,43 @@
-const { axios } = require("./fakeBackend/mock");
+const {
+  axios
+} = require("./fakeBackend/mock");
+
+const {
+  format
+} = require('date-fns');
+
 
 const getFeedbackByProductViewData = async (product, actualize = false) => {
+  try {
+    const response = await axios(`/feedback?product=${product}`);
 
+    if (!response.data.feedback.length) return {
+      message: "Отзывов пока нет"
+    }
+
+    const users = await axios(`/users?ids=${response.data.feedback.map(u => u.userId)}`);
+
+    let {
+      feedback
+    } = response.data;
+
+    if (actualize)
+      feedback = feedback.sort((a, b) => a.date - b.date);
+
+    feedback = feedback.map(r => ({
+      ...r,
+      user: users.data.users.find(u => u.id === r.userId).name + ` (${users.data.users.find(u => u.id === r.userId).email})`,
+      // date: `${format(r.data, 'YYYY.')}`
+    })).sort((a, b) => a.user - b.user);
+
+    return {
+      feedback
+    }
+  } catch (err) {
+    return err.response.data
+  }
 };
 
-module.exports = { getFeedbackByProductViewData };
+module.exports = {
+  getFeedbackByProductViewData
+};
